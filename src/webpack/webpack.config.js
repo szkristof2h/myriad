48bcd7767d2970f34bd8f1628519e20321002407
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-04-20 21:30:36 +0200

    fix .env on client side

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -1,36 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
 
 module.exports = devMode => {
   return {
     entry: {
-      app: './src/app/index.js'
+      app: "./src/app/index.js"
     },
     module: {
       rules: [
         {
           test: /\.js$|\.jsx$/,
-          use: ['babel-loader'],
+          use: ["babel-loader"],
           exclude: /node_modules/
         },
         {
           test: /\.css$/,
           use: [
             {
-              loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader
+              loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader
             },
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: { importLoaders: 1 }
             },
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
                 plugins: [
-                  require('autoprefixer'),
-                  require('postcss-simple-vars'),
-                  require('postcss-nested')
+                  require("autoprefixer"),
+                  require("postcss-simple-vars"),
+                  require("postcss-nested")
                 ]
               }
             }
@@ -38,27 +39,29 @@ module.exports = devMode => {
         },
         {
           test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
+          use: ["file-loader"]
         }
       ]
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/public/index.html',
-        filename: './src/index.html'
+        template: "./src/public/index.html",
+        filename: "./index.html"
       }),
       new MiniCssExtractPlugin({
-        filename: devMode ? '[name].css' : '[name].[hash].css',
-        chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+        filename: devMode ? "[name].css" : "[name].[hash].css",
+        chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.SITE_URL": JSON.stringify(process.env.SITE_URL),
+        "process.env.PORT": JSON.stringify(process.env.PORT)
       })
     ],
     output: {
-      filename: '[name].bundle.js',
-      chunkFilename: '[name].bundle.js',
-      path: path.resolve(__dirname, '../dist'),
-      publicPath: '/'
+      filename: "[name].bundle.js",
+      chunkFilename: "[name].bundle.js",
+      path: path.resolve(__dirname, "../dist"),
+      publicPath: "/"
     }
-  }
+  };
 };
