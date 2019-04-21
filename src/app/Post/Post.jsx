28530c67340aa60cbe55885c29580a2f2bbc12c3
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-04-21 13:59:20 +0200

    make site (image grid) responsive

--- src/app/Post/Post.jsx
+++ src/app/Post/Post.jsx
@@ -17,51 +17,71 @@ const Comments = lazy(() => import('../Comments/Comments.jsx' /* webpackChunkNam
 
 const siteUrl = config.url;
 
-export default function Post({ _id, col, comments, description, dismiss, downs, image, link, openPost, postedByName, rated, row,
-  size, title, type, ups }) {
+export default function Post({
+  _id,
+  col,
+  comments,
+  description,
+  dismiss,
+  downs,
+  image,
+  link,
+  openPost,
+  postedByName,
+  rated,
+  row,
+  size,
+  title,
+  type,
+  ups
+}) {
   const [loading, setLoading] = useState(false);
   const [mounted, setMounted] = useState();
   const { setErrors } = useContext(ErrorContext);
   const { getPost, setPosts } = useContext(PostsContext);
   const url = `${siteUrl}/${_id}`;
 
-  // 
   const rate = (val, e) => {
     e.preventDefault();
     setLoading(true);
 
-    !loading && axios
-      .post(`${siteUrl}/post/post/rating`, { _id, rating: val })
-      .then(res => {
-        setLoading(false);
-        if (res.data.errors) setErrors(errors => [...errors, res.data]);
-        else {
-          if (rated && rated === val)
-            return setPosts(posts => ({
-              ...posts, [_id]: {
-                ...posts[_id],
-                rated: 0, [val > 0 ? 'ups' : 'downs']: val > 0 ? ups - 1 : downs - 1
-              }
-            }));
+    !loading &&
+      axios
+        .post(`${siteUrl}/post/post/rating`, { _id, rating: val })
+        .then(res => {
+          setLoading(false);
+          if (res.data.errors) setErrors(errors => [...errors, res.data]);
+          else {
+            if (rated && rated === val)
+              return setPosts(posts => ({
+                ...posts,
+                [_id]: {
+                  ...posts[_id],
+                  rated: 0,
+                  [val > 0 ? "ups" : "downs"]: val > 0 ? ups - 1 : downs - 1
+                }
+              }));
 
-          setPosts(posts => ({
-            ...posts, [_id]: {
-              ...posts[_id],
-              rated: val, [val > 0 ? 'ups' : 'downs']: val > 0 ? ups + 1 : downs + 1
-            }
-          }));
-          if (rated)
             setPosts(posts => ({
-              ...posts, [_id]: {
+              ...posts,
+              [_id]: {
                 ...posts[_id],
-                [val < 0 ? 'ups' : 'downs']: val < 0 ? ups - 1 : downs - 1
+                rated: val,
+                [val > 0 ? "ups" : "downs"]: val > 0 ? ups + 1 : downs + 1
               }
             }));
-        }
-      })
-      .catch(e => setErrors(errors => [...errors, e.response.data]));
+            if (rated)
+              setPosts(posts => ({
+                ...posts,
+                [_id]: {
+                  ...posts[_id],
+                  [val < 0 ? "ups" : "downs"]: val < 0 ? ups - 1 : downs - 1
+                }
+              }));
+          }
+        })
+        .catch(e => setErrors(errors => [...errors, e.response.data]));
   };
-
 
   const handleClick = e => e.target === e.currentTarget && openPost();
 
@@ -70,111 +90,170 @@ export default function Post({ _id, col, comments, description, dismiss, downs, 
     return () => {
       setMounted(false);
       setLoading(false);
-    }
+    };
   }, []);
 
   useEffect(() => {
     if (!title && mounted) {
       setLoading(true);
-      getPost(_id)
-        .then(() => mounted && setLoading(false));
+      getPost(_id).then(() => mounted && setLoading(false));
     }
-
   }, [mounted, _id]);
 
-  const classType = `post post--${type && type !== 'notification' ? 'stand-alone' : size < 6 ? 'sub2'
-    : size < 18 ? 'sub1' : 'main'}`;
+  const classType = `post post--${
+    type && type !== "notification"
+      ? "stand-alone"
+      : size < 6
+      ? "sub2"
+      : size < 18
+      ? "sub1"
+      : "main"
+  }`;
 
   const postTemplate = (
-    <div className={classType} onClick={e => handleClick(e)} style={{
-      background: `gray url('${_id.length != 20 ? image : sample}') no-repeat center`,
-      backgroundSize: _id.length == 20 ? `70px 70px` : !type ? 'cover' : 'auto auto',
-      gridColumn: `${!type ? '' + col + ' / span ' + size : 'initial'}`,
-      gridRow: `${!type ? '' + row + ' / span ' + size : 'initial'}`
-    }}>
+    <div
+      className={classType}
+      onClick={e => handleClick(e)}
+      style={{
+        background: `gray url('${
+          _id.length != 20 ? image : sample
+        }') no-repeat center`,
+        backgroundSize:
+          _id.length == 20 ? `70px 70px` : !type ? "cover" : "auto auto",
+        gridColumn: `${!type ? "" + col + " / span " + size : "initial"}`,
+        gridRow: `${!type ? "" + row + " / span " + size : "initial"}`
+      }}
+    >
       <div className="post__details">
         <div className="post__personal">
-          <h2 className={"post__title ellipsis"} onClick={e => handleClick(e)}>{title}</h2>
+          <h2 className={"post__title ellipsis"} onClick={e => handleClick(e)}>
+            {title}
+          </h2>
           <h2 className={"post__user ellipsis"}>@ {postedByName}</h2>
         </div>
-        {title !== 'Submit a post!' && <RateButton
-          className={`post__button post__button${rated > 0 ? '--rated' : ''}`}
-          icon={<Star strokeWidth="1.5px" color="white" fill={rated > 0 ? "white" : "none"} />}
-          handleClick={e => rate(1, e)}
-          prefix={'post'}
-          text={ups}
-          to={'impressed'}
-        />}
-        {title !== 'Submit a post!' && <RateButton
-          className={`post__button post__button--meh post__button${rated < 0 ? '--rated' : ''}`}
-          icon={<Meh strokeWidth="1.5px" color="white" />}
-          handleClick={e => rate(-1, e)}
-          prefix={'post'}
-          text={downs}
-          to={'meh'}
-        />}
+        {title !== "Submit a post!" && (
+          <RateButton
+            className={`post__button post__button${rated > 0 ? "--rated" : ""}`}
+            icon={
+              <Star
+                strokeWidth="1.5px"
+                color="white"
+                fill={rated > 0 ? "white" : "none"}
+              />
+            }
+            handleClick={e => rate(1, e)}
+            prefix={"post"}
+            text={ups}
+            to={"impressed"}
+          />
+        )}
+        {title !== "Submit a post!" && (
+          <RateButton
+            className={`post__button post__button--meh post__button${
+              rated < 0 ? "--rated" : ""
+            }`}
+            icon={<Meh strokeWidth="1.5px" color="white" />}
+            handleClick={e => rate(-1, e)}
+            prefix={"post"}
+            text={downs}
+            to={"meh"}
+          />
+        )}
       </div>
     </div>
   );
 
-  return (
-    !type || type === 'notification' ? postTemplate :
-      <Popup show={true} dismiss={dismiss} dismissible={true} modifier="post">
-        <div className="post--stand-alone">
-          <div className="post__image-container">
-            <img className="post__image" src={image} />
-          </div>
-          <a className={"post__title ellipsis"} href={link} target="_blank">{title}</a>
-          <Link className={"post__user ellipsis"} to={`/user/${postedByName}`} >@{postedByName}</Link>
-          <div className="post__summary">
-            {description}
-          </div>
-          <div className="post__buttons">
-            <RateButton
-              className={`post__button post__button--main post__button${rated > 0 ? '--rated' : ''}`}
-              icon={
-                <Star
-                  alt="Impressed!"
-                  className="post__icon post__impressed"
-                  fill={rated > 0 ? "yellow" : "none"}
-                  placeholder="Impressed!"
-                  size="40"
-                  strokeWidth="1.5px" color={rated > 0 ? "yellow" : "gray"}
-                />}
-              handleClick={e => rate(1, e)}
-              prefix={'post'}
-              text={ups}
-              to={'impressed'}
-            />
-            <RateButton
-              className={`post__button post__button--main post__button--meh post__button${rated < 0 ? '--rated' : ''}`}
-              icon={
-                <Meh
-                  alt="Meh..."
-                  className="post__icon post__meh"
-                  color={rated < 0 ? "black" : "gray"}
-                  placeholder="Meh..."
-                  size="40"
-                  strokeWidth="1.5px"
-                />}
-              handleClick={e => rate(-1, e)}
-              prefix={'post'}
-              text={downs}
-              to={'meh'}
-            />
-            <div className="palceholder"></div>
-            <a className="post__button post__button--main" href={`http://www.facebook.com/sharer.php?u=${url}[title]=${title}`}>
-              <Facebook alt="share on facebook" className="post__share-icon post__share-icon--facebook" strokeWidth="1.5px" color="#3b5998" size="40" />
-            </a>
-            <a className="post__button post__button--main" href={`https://twitter.com/share?url=${url}`}>
-              <Twitter alt="share on twitter" className="post__share-icon post__share-icon--twitter" strokeWidth="1.5px" color="#0084b4" size="40" />
-            </a>
-          </div>
-          <Suspense fallback={<div>Loading comments...</div>}>
-            <Comments closePost={dismiss} count={comments} id={_id} type={'post'} />
-          </Suspense>
+  return !type || type === "notification" ? (
+    postTemplate
+  ) : (
+    <Popup show={true} dismiss={dismiss} dismissible={true} modifier="post">
+      <div className="post--stand-alone">
+        <div className="post__image-container">
+          <img className="post__image" src={image} />
         </div>
-      </Popup>
+        <a className={"post__title ellipsis"} href={link} target="_blank">
+          {title}
+        </a>
+        <Link className={"post__user ellipsis"} to={`/user/${postedByName}`}>
+          @{postedByName}
+        </Link>
+        <div className="post__summary">{description}</div>
+        <div className="post__buttons">
+          <RateButton
+            className={`post__button post__button--main post__button${
+              rated > 0 ? "--rated" : ""
+            }`}
+            icon={
+              <Star
+                alt="Impressed!"
+                className="post__icon post__impressed"
+                fill={rated > 0 ? "yellow" : "none"}
+                placeholder="Impressed!"
+                size="40"
+                strokeWidth="1.5px"
+                color={rated > 0 ? "yellow" : "gray"}
+              />
+            }
+            handleClick={e => rate(1, e)}
+            prefix={"post"}
+            text={ups}
+            to={"impressed"}
+          />
+          <RateButton
+            className={`post__button post__button--main post__button--meh post__button${
+              rated < 0 ? "--rated" : ""
+            }`}
+            icon={
+              <Meh
+                alt="Meh..."
+                className="post__icon post__meh"
+                color={rated < 0 ? "black" : "gray"}
+                placeholder="Meh..."
+                size="40"
+                strokeWidth="1.5px"
+              />
+            }
+            handleClick={e => rate(-1, e)}
+            prefix={"post"}
+            text={downs}
+            to={"meh"}
+          />
+          <div className="palceholder" />
+          <a
+            className="post__button post__button--main"
+            href={`http://www.facebook.com/sharer.php?u=${url}[title]=${title}`}
+          >
+            <Facebook
+              alt="share on facebook"
+              className="post__share-icon post__share-icon--facebook"
+              strokeWidth="1.5px"
+              color="#3b5998"
+              size="40"
+            />
+          </a>
+          <a
+            className="post__button post__button--main"
+            href={`https://twitter.com/share?url=${url}`}
+          >
+            <Twitter
+              alt="share on twitter"
+              className="post__share-icon post__share-icon--twitter"
+              strokeWidth="1.5px"
+              color="#0084b4"
+              size="40"
+            />
+          </a>
+        </div>
+        <Suspense fallback={<div>Loading comments...</div>}>
+          <Comments
+            closePost={dismiss}
+            count={comments}
+            id={_id}
+            type={"post"}
+          />
+        </Suspense>
+      </div>
+    </Popup>
   );
 }
 
--- src/app/Posts.jsx
+++ src/app/Posts.jsx
@@ -1,14 +1,17 @@
-import React, { lazy, Suspense, useContext, useEffect, useState } from 'react';
-import { Route } from 'react-router-dom';
-import Proptypes from 'prop-types';
-import { NavigationContext } from './contexts/NavigationContext.jsx';
-import { PostsContext } from './contexts/PostsContext.jsx';
-import { UserContext } from './contexts/UserContext.jsx';
-import Loader from './Loader.jsx';
-import Tags from './Tags.jsx';
-import './posts.css';
+import React, { lazy, Suspense, useContext, useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import Proptypes from "prop-types";
+import { NavigationContext } from "./contexts/NavigationContext.jsx";
+import { PostsContext } from "./contexts/PostsContext.jsx";
+import { UserContext } from "./contexts/UserContext.jsx";
+import useWindowSize from "./hooks/useWindowSize";
+import Loader from "./Loader.jsx";
+import Tags from "./Tags.jsx";
+import "./posts.css";
 
-const Post = lazy(() => import('./Post/Post.jsx' /* webpackChunkName: "Post" */));
+const Post = lazy(() =>
+  import("./Post/Post.jsx" /* webpackChunkName: "Post" */)
+);
 
 const setPos = () => {
   const ratio = [18, 6, 5];
@@ -18,44 +21,87 @@ const setPos = () => {
   const surround = (r, index, a = 1) => {
     const currentRatio = r[1];
     const parentRatio = r[0];
-    const length = Math.floor(parentRatio * a / currentRatio) + 2;
+    const length = Math.floor((parentRatio * a) / currentRatio) + 2;
 
     for (let i = 0; i < length; i++) {
-      positions = [...positions, [(i - 1) * currentRatio - offset, -currentRatio - offset, currentRatio]];
-      positions = [...positions, [(i - 1) * currentRatio - offset, (length - 2) * currentRatio - offset, currentRatio]];
-      if (i != 0 && i != length - 1) positions = [...positions, [-currentRatio - offset, (i - 1) * currentRatio - offset, currentRatio]];
-      if (i != 0 && i != length - 1) positions = [...positions, [(length - 2) * currentRatio - offset, (i - 1) * currentRatio - offset, currentRatio]];
+      positions = [
+        ...positions,
+        [(i - 1) * currentRatio - offset, -currentRatio - offset, currentRatio]
+      ];
+      positions = [
+        ...positions,
+        [
+          (i - 1) * currentRatio - offset,
+          (length - 2) * currentRatio - offset,
+          currentRatio
+        ]
+      ];
+      if (i != 0 && i != length - 1)
+        positions = [
+          ...positions,
+          [
+            -currentRatio - offset,
+            (i - 1) * currentRatio - offset,
+            currentRatio
+          ]
+        ];
+      if (i != 0 && i != length - 1)
+        positions = [
+          ...positions,
+          [
+            (length - 2) * currentRatio - offset,
+            (i - 1) * currentRatio - offset,
+            currentRatio
+          ]
+        ];
     }
 
     index++;
     const newOffset = offset + currentRatio;
     if (r.length !== index - 2) offset = newOffset;
     r.length === index - 2 ? true : surround(r.slice(1), index, length);
-  }
+  };
 
   surround(ratio, 0);
 
   return [positions, offset];
-}
+};
 
 export default function Posts({ fullUrl, history, tag, url, userName }) {
   const [mounted, setMounted] = useState(true);
   const [offset, setOffset] = useState(0);
   const { refresh, setRefresh } = useContext(NavigationContext);
-  const { focused, getPosts, ids, posts, previousUrl, setFocused, setPreviousUrl } = useContext(PostsContext);
+  const {
+    focused,
+    getPosts,
+    ids,
+    posts,
+    previousUrl,
+    setFocused,
+    setPreviousUrl
+  } = useContext(PostsContext);
   const [positions, setPositions] = useState([]);
-  const [loading, setLoading] = useState('');
+  const [loading, setLoading] = useState("");
   const { user } = useContext(UserContext);
-  
+  const { width, height } = useWindowSize();
+
   const openPost = (i, id) => {
     setFocused(id);
-    history.push(id.length != 20 ? `/post/${id}` : user.logged ? user.displayName ? '/add' : '/profile' : '/login');
-  }
+    history.push(
+      id.length != 20
+        ? `/post/${id}`
+        : user.logged
+        ? user.displayName
+          ? "/add"
+          : "/profile"
+        : "/login"
+    );
+  };
 
   const closePost = () => {
     setFocused();
-    history.push(previousUrl ? previousUrl : '/');
-  }
+    history.push(previousUrl ? previousUrl : "/");
+  };
 
   useEffect(() => {
     setMounted(true);
@@ -67,59 +113,94 @@ export default function Posts({ fullUrl, history, tag, url, userName }) {
       setFocused();
       setMounted(false);
       setRefresh(false);
-    }
+    };
   }, []);
 
   useEffect(() => {
-    const type = fullUrl && fullUrl.split("/").length == 3 && fullUrl.split("/")[1] === "post" ?
-      "post" : tag ? tag : userName ? userName : "";
-      `posts${userName ? '/user' : ''}${tag ? '/' + tag.trim() : userName ? '/' + userName : ''}`;
-    
+    const type =
+      fullUrl &&
+      fullUrl.split("/").length == 3 &&
+      fullUrl.split("/")[1] === "post"
+        ? "post"
+        : tag
+        ? tag
+        : userName
+        ? userName
+        : "";
+    `posts${userName ? "/user" : ""}${
+      tag ? "/" + tag.trim() : userName ? "/" + userName : ""
+    }`;
+
     // Checks if url was the previous one (and is not refreshing) to avoid loading unnecessarily
     if ((type !== "post" || !previousUrl) && (previousUrl !== url || refresh)) {
-      setPositions([]);// reset position
+      setPositions([]); // reset position
       setLoading(true);
-        getPosts(`posts${userName ? '/user' : ''}${tag ? '/' + tag.trim() : userName ? '/' + userName : ''}`, type)
-          .then(() => {
-            if (mounted) {
-              const p = setPos();
-              setOffset(p[1]);
-              setPositions(p[0]);
-              setLoading(false);
-              setPreviousUrl(url)
-              setRefresh(false);
-            }
-          });
+      getPosts(
+        `posts${userName ? "/user" : ""}${
+          tag ? "/" + tag.trim() : userName ? "/" + userName : ""
+        }`,
+        type
+      ).then(() => {
+        if (mounted) {
+          const p = setPos();
+          setOffset(p[1]);
+          setPositions(p[0]);
+          setLoading(false);
+          setPreviousUrl(url);
+          setRefresh(false);
+        }
+      });
     }
   }, [refresh, tag, userName]);
 
   return (
     <>
-      <div className="main">
+      <div
+        className="main"
+        style={{
+          maxWidth: `${width - 66}px`,
+          background: "red",
+          gridAutoColumns: `${(width - 66) / 40}px`,
+          gridAutoRows: `${(height - 12) / 40}px`
+        }}
+      >
         <Suspense fallback={<Loader />}>
-          {loading ? <Loader /> : ids.map((id, i) => positions.length > i && <Post
-            key={id}
-            col={positions[i][0] + offset}
-            row={positions[i][1] + offset}
-            openPost={() => openPost(i, id)}
-            size={positions[i][2]}
-            {...posts[id]} />
+          {loading ? (
+            <Loader />
+          ) : (
+            ids.map(
+              (id, i) =>
+                positions.length > i && (
+                  <Post
+                    key={id}
+                    col={positions[i][0] + 1 + offset}
+                    row={positions[i][1] + 1 + offset}
+                    openPost={() => openPost(i, id)}
+                    size={positions[i][2]}
+                    {...posts[id]}
+                  />
+                )
+            )
           )}
         </Suspense>
       </div>
-      <Route exact path="/post/:postId" render={({ match }) =>
-        <Suspense fallback={<Loader />}>
-          <Post
-            dismiss={closePost}
-            _id={match.params.postId}
-            type="standAlone"
-            {...posts[focused]} />
-        </Suspense>
-      }
+      <Route
+        exact
+        path="/post/:postId"
+        render={({ match }) => (
+          <Suspense fallback={<Loader />}>
+            <Post
+              dismiss={closePost}
+              _id={match.params.postId}
+              type="standAlone"
+              {...posts[focused]}
+            />
+          </Suspense>
+        )}
       />
       <Tags />
     </>
-  )
+  );
 }
 
 Posts.propTypes = {
@@ -128,4 +209,4 @@ Posts.propTypes = {
   tag: Proptypes.string,
   url: Proptypes.string,
   userName: Proptypes.string
-}
+};
