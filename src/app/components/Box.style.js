a6342d3c5effcbd418f151ce4ff0aae3d440f66f
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-01 20:03:28 +0200

    create Styled Box component

---
+++ src/app/components/Box.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import theme from "../theme";
+
+const Box = styled.div`
+  justify-self: center;
+  padding: ${theme.base.gutter}px;
+  border-radius: ${theme.base.borderRadius}px;
+  margin: ${theme.base.gutter}px 0;
+  max-width: 550px;
+  min-width: ${({ type }) =>
+    !type ? "100px" : type === "login" ? "300px" : "initial"};
+  background: ${({ type }) =>
+    !type ? "white" : type === "warn" ? theme.color.highRed : "blue"};
+  color: ${({ type }) =>
+    !type ? "black" : type === "warn" ? "white" : "blue"};
+`;
+
+export { Box }
