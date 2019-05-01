be57f20f76b422155dd6fa63b8020b7f5a9fc4c2
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-01 20:04:01 +0200

    create Styled Input component

---
+++ src/app/components/Input.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import theme from "../theme";
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid black;
+  margin-bottom: ${theme.base.gutter*1.5}px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export { Input }
