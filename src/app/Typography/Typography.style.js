fa05f99f6e19e0f59a722e3e180172d2feb7928b
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-11 11:33:03 +0200

    add a Warning component to Typography

--- src/app/Typography/Typography.style.js
+++ src/app/Typography/Typography.style.js
@@ -8,9 +8,17 @@ const Header = styled.div`
   padding: ${theme.base.gutter}px 0;
 `;
 
+const Warning = styled.div`
+  font-size: ${({ size }) => theme.font["header" + size]};
+  text-align: ${({ centered }) => (centered ? "center" : "initial")};
+  font-family: "Artifika";
+  padding: ${theme.base.gutter}px 0;
+  background: ${theme.color.warn}
+`;
+
 const Error = styled.div`
   text-align: ${({ centered }) => (centered ? "center" : "initial")};
   color: ${theme.color.error};
 `;
 
-export { Header, Error };
+export { Header, Error, Warning };
