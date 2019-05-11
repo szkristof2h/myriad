3f8e022aed2dee0e220fb78b9ddf29066a00f78d
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-11 15:59:46 +0200

    change image field in Post schema to array

--- src/server/db/models/Post.js
+++ src/server/db/models/Post.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
-import isURL from 'validator/lib/isURL';
-import { sanitize } from '../../util/misc';
+import mongoose from "mongoose";
+import isURL from "validator/lib/isURL";
+import { sanitize } from "../../util/misc";
 
-const { ObjectId, Schema} = mongoose;
+const { ObjectId, Schema } = mongoose;
 
 const postSchema = new Schema({
   date: { type: Date, default: Date.now },
   description: {
     type: String,
-    maxlength: [300, 'The description can\'t be longer than 300 characters!'],
-    minlength: [3, 'The description should be at least 3 characters!'],
-    required: [true, 'Your post should have a description!'],
+    maxlength: [300, "The description can't be longer than 300 characters!"],
+    minlength: [3, "The description should be at least 3 characters!"],
+    required: [true, "Your post should have a description!"],
     set: v => sanitize(v)
   },
   comments: Number,
@@ -21,15 +21,18 @@ const postSchema = new Schema({
       validator: v => isURL(v),
       message: props => `${props.value} is not a valid url!`
     },
-    required: [true, 'You should give your post a link!']
+    required: [true, "You should give your post a link!"]
   },
   image: {
-    type: String,
+    type: [String],
     validate: {
-      validator: v => isURL(v),
-      message: props => `${props.value} is not a valid url!`
+      validator: images => images.filter(image => !isURL(image)).length === 0,
+      message: props =>
+        `The following images aren't valid: ${props.value
+          .filter(image => !isURL(image))
+          .join(", ")}!`
     },
-    required: [true, 'You should give your post a thumbnail!']
+    required: [true, "You should give your post a thumbnail!"]
   },
   postedById: ObjectId,
   postedByName: String,
@@ -37,12 +40,12 @@ const postSchema = new Schema({
   tags: { type: [String], index: true },
   title: {
     type: String,
-    minLength: [3, 'Your title should be more than 3 characters long!'],
-    maxlength: [50, 'Your title shouldn\'t be more than 50 characters long!'],
-    required: [true, 'You didn\'t post a title!']
+    minLength: [3, "Your title should be more than 3 characters long!"],
+    maxlength: [50, "Your title shouldn't be more than 50 characters long!"],
+    required: [true, "You didn't post a title!"]
   },
   ups: { type: Number, index: true, default: 0 }
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 export default Post;
