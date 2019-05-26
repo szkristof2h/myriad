fc24adf81342d4ee8dd61a291eb636d9ee1daf55
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-26 13:43:48 +0200

    refactor Login component to use Styled components

--- src/app/User/Login.jsx
+++ src/app/User/Login.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Popup from '../Popup.jsx';
 import Google from '../images/Google.jsx';
-import './login.css';
+import { ButtonGoogle } from "../components/Button.style";
+import { Box } from "../components/Box.style";
+import { Header } from "../Typography/Typography.style";
 
 export default function Login() {
   return (
-    <Popup show={true} dismissible={false} >
-      <div className="login box box--basic box--login">
-        <h2 className="login__header">Login</h2>
-        <a href="/auth/google" className="login__button button button--google">
-          <Google size="40" fill="white"  strokeWidth="0" alt="login with google" />
-          <h2 className="button__title">Login with Google</h2>
-        </a>
-      </div>
+    <Popup show={true} dismissible={false}>
+      <Box style={{ zIndex: 100 }}>
+        <Header centered size={3}>
+          Login
+        </Header>
+        <ButtonGoogle
+          as={"a"}
+          href="/auth/google"
+          style={{ display: "grid", gridAutoFlow: "column" }}
+        >
+          <Google
+            size="40"
+            fill="white"
+            strokeWidth="0"
+            alt="login with google"
+          />
+          <Header centered size={2} style={{ fontWeight: 600 }}>
+            Login with Google
+          </Header>
+        </ButtonGoogle>
+      </Box>
     </Popup>
   );
 }
