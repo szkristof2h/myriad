bb8168142ee12faf3278034a63a57a81099f2c32
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-05-26 13:44:39 +0200

    add Style TextArea component

--- src/app/components/Input.style.js
+++ src/app/components/Input.style.js
@@ -10,4 +10,14 @@ const Input = styled.input`
   }
 `;
 
-export { Input }
+const TextArea = styled.textarea`
+  border: none;
+  border-bottom: 1px solid black;
+  margin-bottom: ${theme.base.gutter * 1.5}px;
+  resize: vertical;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export { Input, TextArea }
