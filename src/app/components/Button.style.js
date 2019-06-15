e37b01012c937bc2103ed9567d2c601c6afa7597
Kristóf Szőllősi <szkristof2h@gmail.com>
2019-06-15 15:45:33 +0200

    add new Rate buttons

--- src/app/components/Button.style.js
+++ src/app/components/Button.style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import theme from "../theme";
+import { Header } from "../Typography/Typography.style"
 
 const Button = styled.div`
   padding: ${theme.base.gutter / 2}px;
@@ -54,9 +55,14 @@ const ButtonGoogle = styled(Button)`
 
 const ButtonTransparent = styled(Button)`
   background: transparent;
+  color: black;
   &:hover {
     background: transparent;
     text-decoration: underline;
+    color: black;
+  }
+  &:active, &:visited {
+    color: black;
   }
 `;
 
@@ -64,11 +70,64 @@ const ButtonActive = styled(Button)`
   background: rgb(151, 149, 87);
 `;
 
+const ButtonRate = styled(Button)`
+  display: grid;
+  grid-template-columns: 24px 1fr;
+  grid-column-gap: ${theme.base.gutter / 2}px;
+  align-self: end;
+  align-items: center;
+  height: 26px;
+  padding: 1px 2px;
+  background: ${({ type }) => theme.color[type]};
+  &:hover {
+    background: ${({ type }) => theme.color[type + "Hover"]};
+  }
+  > .icon {
+    width: 20px;
+  }
+  ${Header} {
+    padding: 0;
+    font-family: "Artifika";
+  }
+`;
+
+const ButtonRateBig = styled(Button)`
+  display: grid;
+  background: none;
+  grid-template-columns: auto 1fr;
+  grid-column-gap: ${theme.base.gutter / 2}px;
+  align-self: end;
+  align-items: center;
+  height: auto;
+  padding: 0;
+  > .icon {
+    width: 40px;
+  }
+  ${Header} {
+    color: ${({ rated }) => (rated ? "black" : "gray")};
+    padding: 0 ${theme.base.gutter/2}px 0 0;
+  }
+  &:hover {
+    background: none;
+    & > ${Header} {
+      color: ${({ rated }) => (!rated ? "black" : "gray")};
+    }
+    & > .icon {
+      stroke: ${({ rated, type }) =>
+        rated ? "gray" : type === "impressed" ? "yellow" : "black"}
+      fill: ${({ rated, type }) =>
+        rated ? "none" : type === "impressed" ? "yellow" : "none"}
+    }
+  }
+`;
+
 export {
   Button,
   ButtonError,
   ButtonOk,
   ButtonGoogle,
+  ButtonRate,
+  ButtonRateBig,
   ButtonTransparent,
   ButtonActive,
   ButtonArrow
--- src/app/theme.js
+++ src/app/theme.js
@@ -2,12 +2,19 @@ const color = {
   brown: "rgb(177, 175, 101)",
   brownLight: "rgb(210, 207, 118)",
   brownDark: "rgb(151, 149, 87)",
-  red: "red",
-  redHigh: "rgb(206, 37, 37)",
-  redHighHover: "rgb(236, 43, 43)",
   green: "green",
   greenHigh: "rgb(47, 228, 41)",
   greenHighHover: "rgb(47, 228, 41)",
+  greenNormal: "rgba(0, 128, 0, 0.6)",// find better names
+  greenNormalHover: "rgba(0, 128, 0, 0.4)",// find better names
+  lightGray: "rgb(233, 228, 253)",
+  gray: "rgba(128, 128, 128, 0.5)",
+  grayer: "rgba(128, 128, 128, 0.7)",
+  red: "red",
+  redHigh: "rgb(206, 37, 37)",
+  redHighHover: "rgb(236, 43, 43)",
+  redNormal: "rgba(255, 0, 0, 0.6)",// find better names
+  redNormalHover: "rgba(255, 0, 0, 0.4)",// find better names
   yellow: "yellow"
 };
 
@@ -18,6 +25,7 @@ export default {
   },
   font: {
     size: 16,
+    ["header-1"]: "0.75em",
     header1: "1em",
     header2: "1.25em",
     header3: "1.5em",
@@ -27,12 +35,20 @@ export default {
   },
   color: {
     ...color,
+    divider: color.gray,
     error: color.redHigh,
     errorHover: color.redHighHover,
     errorActive: color.redHighHover,
+    impressed: color.greenNormal,
+    impressedHover: color.greenNormalHover,
+    meh: color.redNormal,
+    mehHover: color.redNormalHover,
+    rateActive: color.grayer,
+    rateActiveHover: color.gray,
     ok: color.greenHigh,
     okHover: color.greenHighHover,
     okActive: color.greenHighHover,
-    warn: color.yellow
+    warn: color.yellow,
+    zebraRow: color.lightGray
   }
 };
