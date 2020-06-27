b83d21fb84ec1380039c55273e35ec8eea12fcec
Szollosi Kristof <szkristof2h@gmail.com>
2020-06-27 18:37:10 +0200

    fix Input component's type

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -1,16 +1,9 @@
 import React, { FC, ChangeEvent } from "react"
 import StyledInput from "./styles"
 
-interface Props {
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
-  onKeyPress?: (event: React.KeyboardEvent<HTMLInputElement>) => void
-  value?: string
-  className?: string
-  placeholder?: string
-}
+interface Props extends React.HTMLProps<HTMLInputElement> {}
 
-const Input:FC<Props> = props => {
-
+const Input: FC<Props> = props => {
   return <StyledInput {...props} />
 }
 
