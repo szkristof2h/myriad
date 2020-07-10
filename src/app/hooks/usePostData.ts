a5a93c305fe1af57865f219c041c7d74ee210456
Szollosi Kristof <szkristof2h@gmail.com>
2020-07-10 18:56:31 +0200

    fix usePostData type

--- src/app/hooks/usePostData.ts
+++ src/app/hooks/usePostData.ts
@@ -3,25 +3,22 @@ import { post, APIPostRequestInteface } from "../requests/api"
 import { ErrorContext } from "../contexts/ErrorContext"
 import { Canceler } from "axios"
 
-export interface PostData<T> {
+export interface PostData<T, V> {
   cancel: Canceler
   data: T | undefined
   isLoading: boolean
   refetch: () => void
-  startPost: () => Promise<void>
+  startPost: (variables: V) => Promise<void>
 }
 
-const usePostData = <T = any, V = any>(
-  url: string,
-  variables: V
-): PostData<T> => {
+const usePostData = <T = any, V = any>(url: string): PostData<T, V> => {
   const [data, setData] = useState<T>()
   const [isLoading, setIsLoading] = useState(false)
   const [cancel, setCancel] = useState<Canceler>(() => (message: string) => {})
   const [shouldRefetch, setShouldRefetch] = useState(false)
   const { addError } = useContext(ErrorContext)
 
-  const startPost = async () => {
+  const startPost = async (variables: V) => {
     setIsLoading(true)
 
     const { postData, cancel, getHasFailed }: APIPostRequestInteface<T> = post<
