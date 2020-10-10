bef881b1d74e5279fb48210682a8ce401333d9a3
Szollosi Kristof <szkristof2h@gmail.com>
2020-10-10 23:03:55 +0200

    readd conditional port

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,5 @@
 export default {
-  url: `${process.env.SITE_URL}:${process.env.PORT}`,
+  url: `${process.env.SITE_URL}${
+    process.env.NODE_ENV !== "production" ? ":" + process.env.PORT : ""
+  }`,
 }
