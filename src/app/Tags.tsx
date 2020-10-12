c591a720030d132ca8585adb90291aa7ef7cb1b4
Szollosi Kristof <szkristof2h@gmail.com>
2020-10-12 19:39:29 +0200

    use hook for requests in Tags

--- src/app/Tags.tsx
+++ src/app/Tags.tsx
@@ -1,51 +1,15 @@
-import React, { useContext, useEffect, useState } from "react"
-import { get, APIRequestInteface } from "./requests/api"
+import React from "react"
 import { Link } from "react-router-dom"
-import { ErrorContext } from "./contexts/ErrorContext"
 import { StyledTags } from "./Tags.style"
-
-interface TagsDataInterface extends APIRequestInteface<GetTagsData> {}
+import useGetData from "./hooks/useGetData"
 
 interface GetTagsData {
-  ids: string[]
   tags: string[]
 }
 
 const Tags = () => {
-  const [tags, setTags] = useState<string[]>([])
-  const { addError } = useContext(ErrorContext)
-
-  const getTags = () => {
-    const { getData, cancel, getHasFailed }: TagsDataInterface = get(
-      "tags",
-      () => addError({ tags: ["some error message here"] })
-    )
-
-    const setAllTags = async () => {
-      const response = await getData()
-
-      if (getHasFailed() || !response)
-        return addError({ posts: [`get posts request failed`] })
-
-      const {
-        data: { error, tags: tagsData },
-      } = response
-
-      if (error) return addError(error.message, error.type)
-
-      setTags(tagsData)
-    }
-
-    return { cancel, setAllTags }
-  }
-
-  useEffect(() => {
-    const { cancel, setAllTags } = getTags()
-
-    ;(async () => await setAllTags())()
-
-    return cancel
-  }, [])
+  const { data, isLoading } = useGetData<GetTagsData>("tags")
+  const tags = data?.tags
 
   return (
     <StyledTags className="tags">
