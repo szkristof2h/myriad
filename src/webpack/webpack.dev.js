60fb3dc5327ccdae346aa516634e7de00ec8b586
Szollosi Kristof <szkristof2h@gmail.com>
2020-11-07 13:40:44 +0100

    fix webpack watch not working

--- src/webpack/webpack.dev.js
+++ src/webpack/webpack.dev.js
@@ -4,4 +4,8 @@ const common = require("./webpack.config.js")
 module.exports = merge(common(true), {
   mode: "development",
   devtool: "inline-source-map",
+  watchOptions: {
+    aggregateTimeout: 500, // delay before reloading
+    poll: 1000, // enable polling since fsevents are not supported in docker
+  },
 })
